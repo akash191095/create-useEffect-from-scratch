f42602be81dd0914bc63feaadf86e74d9e5be678
Akash Agarwal <akash191095@gmail.com>
2022-06-04 18:04:25 +0530

    add custom useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import root (needed for react v18)
+import root from "./index";
+
+// define global value
+let value;
+
+function useState(initialValue) {
+  // set initial value
+  if (typeof value === "undefined") value = initialValue;
+  // updater function
+  function setState(nextValue) {
+    value = nextValue;
+    // rerender component with new value
+    root.render(<App />);
+  }
+  // return useState hook values
+  return [value, setState];
+}
 
 function App() {
+  const [name, setName] = useState("");
+  function handleChange(evt) {
+    setName(evt.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My name is: {name}</h1>
+      <input type="text" value={name} onChange={handleChange} />
     </div>
   );
 }
